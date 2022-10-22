bfc7f4fe6625c742ab6be73bf1cb7ce13c390abc
olawalekareemdev <olawalekareemdev@gmail.com>
2022-10-22 19:09:35 +0100

    feat: dockerize application

--- TechNews/news/views.py
+++ TechNews/news/views.py
@@ -1,5 +1,16 @@
+from django.views import generic
+# from .models import Post
+from django.http import HttpResponse
 
-from rest_framework.viewsets import ModelViewSet
+
+# class PostList(generic.ListView):
+#     queryset = Post.objects.filter(status=1).order_by('-created_on')
+#     template_name = 'index.html'
+
+# class PostDetail(generic.DetailView):
+#     model = Post
+#     template_name = 'post_detail.html'
 
 
-
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
